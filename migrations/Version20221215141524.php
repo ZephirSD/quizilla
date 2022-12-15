460fca51ce3a392d16b2ada156d90dd7f070f43b
ZephirSD <siegfrieddallery@gmail.com>
2022-12-15 20:40:52 +0400

    Migration pour la base de données du 15122022

--- migrations/Version20221215141524.php
+++ migrations/Version20221215141524.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221215103523 extends AbstractMigration
+final class Version20221215141524 extends AbstractMigration
 {
     public function getDescription(): string
     {
