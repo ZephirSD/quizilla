ff95c3a7fb3ce99582275adfbf7d98eafabff306
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-12-16 15:54:50 +0400

    Mise en place de la redirection lors de la connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -59,12 +59,12 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            $user->setRoles(["ROLE_ADMIN"]);
+            $user->setRoles(["ROLE_PRO"]);
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_quiz_index');
         }
 
         return $this->render('registration/register.html.twig', [
