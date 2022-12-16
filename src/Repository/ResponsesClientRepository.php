3cc1d75ef6bd88f027b1c16cee00ecc2fcaa51bf
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-12-16 16:02:45 +0400

    Création de la ResponsesClientRepository

--- src/Repository/ResponsesClientRepository.php
+++ src/Repository/ResponsesClientRepository.php
@@ -39,28 +39,28 @@ class ResponsesClientRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return ResponsesClient[] Returns an array of ResponsesClient objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return ResponsesClient[] Returns an array of ResponsesClient objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('r.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?ResponsesClient
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?ResponsesClient
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
